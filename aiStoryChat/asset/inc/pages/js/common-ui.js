00d5ed34da4eb370d5b88a5d543e7d66bac97914
조찬기 <36979670+isuda7@users.noreply.github.com>
2025-08-06 09:26:38 +0900

    스토리챗 메인

--- aiStoryChat/asset/inc/pages/js/common-ui.js
+++ aiStoryChat/asset/inc/pages/js/common-ui.js
@@ -1,25 +1,11 @@
 $(function(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const tabIdFromUrl = urlParams.get('tabId');
 
-    //채널 dropbox
-    let dropTarget = $('.float-contents .drop-box h2');
-    let dropBox = $('.float-contents .drop-box ul');
-    let dropBoxItem = $('.float-contents .drop-box ul li a');
-    $(dropTarget).on('click' , function(){
-        $(dropBox).slideToggle();
-    });
-    $(dropBoxItem).on('click' , function(){
-        $(dropBox).slideUp();
-    });
-
-    $(window).resize(function(){ 
-        if (window.innerWidth < 960) { 
-            $('.responsive-layout').addClass('wrapper-m');
-            
-            } else {
-                $('.responsive-layout').removeClass('wrapper-m');   
-                $('.only-mobile').hide();
-            
-            }
-        
-    }).resize(); 
+    if (tabIdFromUrl) {
+        const targetElement = document.getElementById(tabIdFromUrl);
+        if (targetElement) {
+            targetElement.click();
+        }
+    }
 })
