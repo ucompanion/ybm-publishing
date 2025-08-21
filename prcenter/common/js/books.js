e7e851f514d8d498bbd1c7d2e89ec1861a8f5e1d
조찬기 <36979670+isuda7@users.noreply.github.com>
2025-08-21 17:15:41 +0900

    특장점 및 집필진 적용

--- prcenter/common/js/books.js
+++ prcenter/common/js/books.js
@@ -477,7 +477,7 @@ $(function(){
         $('.book-tab-menu .item:nth(2)').remove();
     }
     // 특정 교과서 전자 저작물 제외
-    const arBooksList02 = new Array('ele9','mid12','mid13','mid14','hi15','hi16','hi17','hi19');
+    const arBooksList02 = new Array('ele9','mid12','mid13','mid14','hi9','hi15','hi16','hi17','hi19');
     if(arBooksList02.indexOf(bookCode) >= 0){
         $('.book-tab-menu .item:last-child').remove();
     }
